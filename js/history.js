56c0c252c7c4b50a1557a65f5c7c05497af14078
loicbdev <loicbodolec@msn.com>
2020-10-07 12:16:55 +0200

    MAJ bouton Voir plus en version mobile avec du JS

--- js/history.js
+++ js/history.js
@@ -1,9 +1,11 @@
 let togg1 = document.getElementById("togg1");
 let togg2 = document.getElementById("togg2");
 let togg3 = document.getElementById("togg3");
+
 let d1 = document.getElementById("d1");
 let d2 = document.getElementById("d2");
 let d3 = document.getElementById("d3");
+
 togg1.addEventListener("click", () => {
   if(getComputedStyle(d1).display != "none"){
     d1.style.display = "none";
@@ -28,3 +30,40 @@ togg3.addEventListener("click", () => {
   }
 });
 
+
+
+togg1.addEventListener("click", updateBtn1);
+
+function updateBtn1() {
+  if (togg1.value === 'Voir plus') {
+    togg1.value = 'Voir moins';
+    
+  } else {
+    togg1.value = 'Voir plus';
+    
+  }
+};
+
+togg2.addEventListener("click", updateBtn2);
+
+function updateBtn2() {
+  if (togg2.value === 'Voir plus') {
+    togg2.value = 'Voir moins';
+    
+  } else {
+    togg2.value = 'Voir plus';
+    
+  }
+};
+
+togg3.addEventListener("click", updateBtn3);
+
+function updateBtn3() {
+  if (togg3.value === 'Voir plus') {
+    togg3.value = 'Voir moins';
+    
+  } else {
+    togg3.value = 'Voir plus';
+    
+  }
+};
